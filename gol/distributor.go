f8ae3b4120f26acf60c4b2af6c783d68e38b5fed
willbowden <willbowden03@yahoo.co.uk>
2023-11-07 16:57:58 +0000

    Make 2D slice for world

--- gol/distributor.go
+++ gol/distributor.go
@@ -12,22 +12,24 @@ type distributorChannels struct {
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
-	// TODO: Create a 2D slice to store the world.
-
-	turn := 0
-
+	var world = make([][]byte, p.ImageHeight)
+	for i := 0; i < p.ImageHeight; i++ {
+		world[i] = make([]byte, p.ImageWidth)
+	}
 
 	// TODO: Execute all turns of the Game of Life.
+	for turn := 0; turn <= p.Turns; turn++ {
+
+	}
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
 }
---
+++ gol/worker.go
@@ -0,0 +1,5 @@
+package gol
+
+func worker(y1, y2, x1, x2 int, board func(x, y int) uint8, c chan<- Event) {
+
+}
