b5a695739d6ecfa33adb58c5c876c87a44d834c5
willbowden <willbowden03@yahoo.co.uk>
2023-11-07 18:06:52 +0000

    Starting on part 1

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,11 @@
 package gol
 
+import (
+	"fmt"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,20 +15,74 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+func makeImmutableWorld(world [][]uint8) func(y, x int) uint8 {
+	return func(y, x int) uint8 {
+		return world[y][x]
+	}
+}
+
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	var liveCells []util.Cell
+	for y, row := range world {
+		for x, cellValue := range row {
+			if cellValue == 255 {
+				liveCells = append(liveCells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+
+	return liveCells
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
+	fmt.Println("HELLO")
+
+	c.events <- StateChange{CompletedTurns: 0, NewState: Executing}
+
 	var world = make([][]byte, p.ImageHeight)
-	for i := 0; i < p.ImageHeight; i++ {
-		world[i] = make([]byte, p.ImageWidth)
+	// Start IO image reading
+	filename := fmt.Sprintf("%vx%v", p.ImageWidth, p.ImageHeight)
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
+	// Read each cell into our world
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			cell := <-c.ioInput
+			world[y][x] = cell
+		}
 	}
 
-	// TODO: Execute all turns of the Game of Life.
-	for turn := 0; turn <= p.Turns; turn++ {
+	fmt.Println("Read file!")
 
+	turn := 0
+	for turn = 0; turn < p.Turns; turn++ {
+		fmt.Println("Calculating turn ", turn)
+		// Make new 2D array for the next frame
+		var newFrame [][]uint8
+		workerChannel := make(chan [][]uint8)
+		for i := 0; i < p.Threads; i++ {
+			// Divide up the world and send to our workers
+			y1 := i * (p.ImageHeight / p.Threads)
+			y2 := ((i + 1) * (p.ImageHeight / p.Threads))
+			go worker(y1, y2, makeImmutableWorld(world), c.events, workerChannel, p, turn)
+		}
+		for j := 0; j < p.Threads; j++ {
+			// Retrieve the new slices from the workers & append into new frame
+			newSlice := <-workerChannel
+			newFrame = append(newFrame, newSlice...)
+		}
+		// Overwrite the world with our new frame
+		for col := range newFrame {
+			copy(newFrame[col], world[col])
+		}
+
+		// c.events <- TurnComplete{CompletedTurns: turn}
 	}
 
-	// TODO: Report the final state using FinalTurnCompleteEvent.
+	liveCells := calculateAliveCells(p, world)
+	c.events <- FinalTurnComplete{CompletedTurns: turn, Alive: liveCells}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
--- gol/worker.go
+++ gol/worker.go
@@ -1,5 +1,49 @@
 package gol
 
-func worker(y1, y2, x1, x2 int, board func(x, y int) uint8, c chan<- Event) {
+import "uk.ac.bris.cs/gameoflife/util"
 
+func getNumNeighbours(x int, y int, world func(y, x int) uint8, p Params) int {
+	numNeighbours := 0
+	for yInc := -1; yInc <= 1; yInc++ {
+		var testY int = (y + yInc + p.ImageHeight) % p.ImageHeight
+		for xInc := -1; xInc <= 1; xInc++ {
+			var testX int = (x + xInc + p.ImageWidth) % p.ImageWidth
+			if (testX != x || testY != y) && world(testY, testX) == 255 {
+				numNeighbours++
+			}
+		}
+	}
+
+	return numNeighbours
+}
+
+func setCell(x, y int, world func(y, x int) uint8, newValue uint8, events chan<- Event, turn int) {
+	if world(y, x) != newValue {
+		events <- CellFlipped{CompletedTurns: turn, Cell: util.Cell{X: x, Y: y}}
+	}
+}
+
+func worker(y1, y2 int, world func(y, x int) uint8, events chan<- Event, c chan<- [][]uint8, p Params, turn int) {
+	sliceHeight := (y2 - y1) + 1
+	var newSlice = make([][]uint8, sliceHeight)
+	for i := 0; i < sliceHeight; i++ {
+		newSlice[i] = make([]uint8, p.ImageWidth)
+	}
+	for y := y1; y < y2; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			neighbours := getNumNeighbours(x, y, world, p)
+			switch {
+			case neighbours < 2:
+				setCell(x, y, world, 0, events, turn)
+				newSlice[y][x] = 0
+			case neighbours == 3:
+				setCell(x, y, world, 255, events, turn)
+				newSlice[y][x] = 255
+			case neighbours > 3:
+				setCell(x, y, world, 0, events, turn)
+				newSlice[y][x] = 0
+			}
+		}
+	}
+	c <- newSlice
 }
