85c7e34783e29a7b5ef4944284d28590a0bf88e6
Will <willbowden03@yahoo.co.uk>
2023-11-28 21:35:21 +0000

    Added AWS node private IPs

--- benchmark_plot.py
+++ benchmark_plot.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 data = pd.read_csv('results.csv', header=0, names=['name', 'time', 'range'])
 # convert seconds from ns 
-data['time'] /= 1e+9
+# data['time'] /= 1e+9
 
 data['threads'] = data['name'].str.extract('Gol/\d+x\d+x\d+-(\d+)-\d+').apply(pd.to_numeric)
 data['cpu_cores'] = data['name'].str.extract('Gol/\d+x\d+x\d+-\d+-(\d+)').apply(pd.to_numeric)
@@ -14,13 +14,13 @@ data['cpu_cores'] = data['name'].str.extract('Gol/\d+x\d+x\d+-\d+-(\d+)').apply(
 print(data)
 
 # Plot a bar chart.
-ax = sns.barplot(data=data, x='threads', y='time', hue='cpu_cores', ci=None)
+ax = sns.barplot(data=data, x='threads', y='time', ci=None)
 
 # Set descriptive axis labels.
 ax.set(xlabel='Worker threads used', ylabel='Time taken (s)')
 
 # Display the legend
-ax.legend(title='CPU Cores')
+# ax.legend(title='CPU Cores')
 
 # Display the full figure.
 plt.show()
